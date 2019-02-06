9125400e9b0662e3d30863a5d0e081e8db25af16
Leon Foks <leonfoks@gmail.com>
2019-02-06 16:45:06 -0700

    Rearranged meshes.  Added 2D mesh with a draped top surface.

---
+++ geobipy/src/classes/mesh/TopoRectilinearMesh2D.py
@@ -0,0 +1,259 @@
+""" @TopoRectilinearMesh2D_Class
+Module describing a 2D Rectilinear Mesh class with x and y axes specified.  The upper surface of the mesh can be draped.
+"""
+from copy import deepcopy
+from ...classes.core.StatArray import StatArray
+from .RectilinearMesh1D import RectilinearMesh1D
+from .RectilinearMesh2D import RectilinearMesh2D
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.collections import LineCollection
+from ...base import customPlots as cP
+from ...base import fileIO as fio
+
+try:
+    from pyvtk import VtkData, CellData, Scalars, PolyData
+except:
+    pass
+
+class TopoRectilinearMesh2D(RectilinearMesh2D):
+    """Class defining a 2D rectilinear mesh with cell centres and edges and an upper surface that is draped along
+    an elevation profile.
+
+    Contains a simple 2D mesh with cell edges, widths, and centre locations.
+    In the instantiation, there are three cartesian co-ordinates, x, y, z, specified for the cell centres or edges.
+    This allows you to, for example, create a vertical 2D mesh that is not parallel to either the 
+    x or y axis, like a collection line of data.
+    The mesh lies in the x-z plane.  The y axis can be used to compute the distance sqrt(x^2 + y^2), 
+    which can then be used for plotting.
+    A height profile can also be given that specifies the height of the upper most cell. 
+    The cell locations in z are then added to the height profile.
+
+    TopoRectilinearMesh2D(xCentres, xEdges, yCentres, yEdges, zCentres, zEdges, heightCentres, heightEdges)
+
+    Parameters
+    ----------
+    xCentres : geobipy.StatArray, optional
+        The locations of the centre of each cell in the "x" direction. Only xCentres or xEdges can be given, not both.
+    xEdges : geobipy.StatArray, optional
+        The locations of the edges of each cell, including the outermost edges, in the "x" direction. Only xCentres or xEdges can be given, not both.
+    yCentres : geobipy.StatArray, optional
+        The locations of the centre of each cell in the "y" direction. Only yCentres or yEdges can be given, not both.
+    yEdges : geobipy.StatArray, optional
+        The locations of the edges of each cell, including the outermost edges, in the "y" direction. Only yCentres or yEdges can be given, not both.
+    zCentres : geobipy.StatArray, optional
+        The locations of the centre of each cell in the "z" direction. Only zCentres or zEdges can be given, not both.
+    zEdges : geobipy.StatArray, optional
+        The locations of the edges of each cell, including the outermost edges, in the "z" direction. Only zCentres or zEdges can be given, not both.
+    heightCentres : geobipy.StatArray, optional
+        The height of each point at the x, y locations. Only heightCentres or heightEdges can be given, not both.
+    heightEdges : geobipy.StatArray, optional
+        The height of each point at the x, y locations of the edges of each cell, including the outermost edges. Only heightCentres or heightEdges can be given, not both.
+
+    Returns
+    -------
+    out : TopoRectilinearMesh2D
+        The 2D mesh.
+
+    """
+
+
+    def __init__(self, xCentres=None, xEdges=None, yCentres=None, yEdges=None, zCentres=None, zEdges=None, heightCentres=None, heightEdges=None):
+        """ Initialize a 2D Rectilinear Mesh"""
+
+        if (all(x is None for x in [xCentres, xEdges, yCentres, yEdges, zCentres, zEdges, heightCentres, heightEdges])):
+            return
+
+        super().__init__(xCentres, xEdges, yCentres, yEdges, zCentres, zEdges)
+
+        # mesh of the z axis values
+        self.height = RectilinearMesh1D(cellCentres=heightCentres, cellEdges=heightEdges)
+
+        assert self.height.nCells == self.x.nCells, Exception("heights must have enough values for {} cells or {} edges.".format(self.x.nCells, self.x.nEdges))
+
+        self.setXMesh()
+        self.zMesh()
+
+
+    def XMesh(self, xAxis='x'):
+        """Creates an array suitable for plt.pcolormesh for the abscissa.
+        
+        Parameters
+        ----------
+        xAxis : str
+            If axis is 'x', the horizontal axis uses self.x
+            If axis is 'y', the horizontal axis uses self.y
+            If axis is 'r', the horizontal axis uses cumulative distance along the line.
+
+        """
+
+        assert xAxis in ['x', 'y', 'r'], Exception("axis must be either 'x', 'y' or 'r'")
+        if xAxis == 'x':
+            xMesh = np.repeat(self.x.cellEdges[np.newaxis, :], self.z.nCells+1, 0)
+        elif xAxis == 'y':
+            assert self.xyz, Exception("To plot against 'y' the mesh must be instantiated with three co-ordinates")
+            xMesh = np.repeat(self.y.cellEdges[np.newaxis, :], self.z.nCells+1, 0)
+        elif xAxis == 'r':
+            assert self.xyz, Exception("To plot against 'r' the mesh must be instantiated with three co-ordinates")
+            dx = np.diff(self.x.cellEdges)
+            dy = np.diff(self.y.cellEdges)
+            distance = StatArray(np.zeros(self.x.nEdges), 'Distance', self.x.cellCentres.units)
+            distance[1:] = np.cumsum(np.sqrt(dx**2.0 + dy**2.0))
+            xMesh = np.repeat(distance[np.newaxis, :], self.z.nCells+1, 0)
+
+        return xMesh
+
+    
+    def setXMesh(self, xAxis='x'):
+
+        self._xMesh = self.XMesh(xAxis=xAxis)
+
+
+    def zMesh(self):
+        """Creates an array suitable for plt.pcolormesh for the ordinate """
+        self._zMesh = self.height.cellEdges - np.repeat(self.z.cellEdges[:, np.newaxis], self.x.nCells+1, 1)
+
+
+    def pcolor(self, values, xAxis='x', **kwargs):
+        """Create a pseudocolour plot of a 2D array using the mesh.
+
+        Parameters
+        ----------
+        values : array_like or StatArray
+            A 2D array of colour values.
+        axis : str
+            If axis is 'x', the horizontal axis uses self.x
+            If axis is 'y', the horizontal axis uses self.y
+            If axis is 'r', the horizontal axis uses cumulative distance along the line
+
+        Other Parameters
+        ----------------
+        alpha : scalar or array_like, optional
+            If alpha is scalar, behaves like standard matplotlib alpha and opacity is applied to entire plot
+            If array_like, each pixel is given an individual alpha value.
+        log : 'e' or float, optional
+            Take the log of the colour to a base. 'e' if log = 'e', and a number e.g. log = 10.
+            Values in c that are <= 0 are masked.
+        equalize : bool, optional
+            Equalize the histogram of the colourmap so that all colours have an equal amount.
+        nbins : int, optional
+            Number of bins to use for histogram equalization.
+        xscale : str, optional
+            Scale the x axis? e.g. xscale = 'linear' or 'log'
+        yscale : str, optional
+            Scale the y axis? e.g. yscale = 'linear' or 'log'.
+        flipX : bool, optional
+            Flip the X axis
+        flipY : bool, optional
+            Flip the Y axis
+        grid : bool, optional
+            Plot the grid
+        noColorbar : bool, optional
+            Turn off the colour bar, useful if multiple customPlots plotting routines are used on the same figure.   
+        trim : bool, optional
+            Set the x and y limits to the first and last non zero values along each axis.    
+
+        Returns
+        -------
+        ax
+            matplotlib .Axes
+
+        See Also
+        --------
+        matplotlib.pyplot.pcolormesh : For additional keyword arguments you may use.
+
+        """
+        # assert isinstance(values, StatArray), TypeError("values must be a StatArray")
+        assert np.all(values.shape == self.dims), ValueError("values must have shape {}".format(self.dims))
+
+        self.setXMesh(xAxis=xAxis)
+        pm = cP.pcolormesh(self._xMesh, self._zMesh, values, **kwargs)
+        cP.xlabel(self._xMesh.getNameUnits())
+        cP.ylabel(self.z.cellCentres.getNameUnits())
+
+    
+    def plotGrid(self, xAxis='x', **kwargs):
+        """Plot the grid lines of the mesh. """
+
+        xscale = kwargs.pop('xscale', 'linear')
+        yscale = kwargs.pop('yscale', 'linear')
+        flipX = kwargs.pop('flipX', False)
+        flipY = kwargs.pop('flipY', False)
+        c = kwargs.pop('color', 'k')
+
+        xtmp = super().getXAxis(xAxis)
+
+        ax = plt.gca()
+        cP.pretty(ax)
+        ax.vlines(x = xtmp, ymin=self._zMesh[0, :], ymax=self._zMesh[-1, :], **kwargs)
+        segs = np.zeros([self.z.nEdges, self.x.nEdges, 2])
+        segs[:, :, 0] = np.repeat(xtmp[np.newaxis, :], self.z.nEdges, 0)
+        segs[:, :, 1] = self.height.cellEdges - np.repeat(self.z.cellEdges[:, np.newaxis], self.x.nEdges, 1)
+
+        ls = LineCollection(segs, color='k', linestyle='solid', **kwargs)
+        ax.add_collection(ls)
+
+        dz = 0.02 * np.abs(xtmp.max() - xtmp.min())
+        ax.set_xlim(xtmp.min() - dz, xtmp.max() + dz)
+        dz = 0.02 * np.abs(self._zMesh.max() - self._zMesh.min())
+        ax.set_ylim(self._zMesh.min() - dz, self._zMesh.max() + dz)
+
+
+        plt.xscale(xscale)
+        plt.yscale(yscale)
+        cP.xlabel(xtmp.getNameUnits())
+        cP.ylabel(self.y._cellCentres.getNameUnits())
+        
+        if flipX:
+            ax.set_xlim(ax.get_xlim()[::-1])
+
+        if flipY:
+            ax.set_ylim(ax.get_ylim()[::-1])
+
+
+    def plotHeight(self, xAxis='x', centres=False, **kwargs):
+        """Plot the height of the mesh as a line. """
+
+        kwargs['c'] = kwargs.pop('color', 'k')
+        kwargs['linewidth'] = kwargs.pop('linewidth', 1.0)
+
+        xtmp = super().getXAxis(xAxis, centres=centres)
+
+        if centres:
+            self.height.cellCentres.plot(xtmp, **kwargs)
+        else:
+            self.height.cellEdges.plot(xtmp, **kwargs)
+                
+
+    def vtkStructure(self):
+        """Generates a vtk mesh structure that can be used in a vtk file.
+
+        Returns
+        -------
+        out : pyvtk.VtkData
+            Vtk data structure
+
+        """
+        # Generate the quad node locations in x
+        x = self.x.cellEdges
+        y = self.y.cellEdges
+        z = self.z.cellEdges
+
+        nCells = self.nCells
+
+        z = self.z.cellEdges
+        nNodes = self.x.nEdges * self.z.nEdges
+
+        # Constuct the node locations for the vtk file
+        nodes = np.empty([nNodes, 3])
+
+        nodes[:, 0] = self.XMesh('x').reshape(self.nNodes)
+        nodes[:, 1] = self.XMesh('y').reshape(self.nNodes)
+        nodes[:, 2] = self._zMesh.reshape(self.nNodes)
+
+        tmp = np.int32([0, 1, self.x.nEdges+1, self.x.nEdges])
+        a = np.ones(self.x.nCells, dtype=np.int32)
+        a[0] = 2
+        index = (np.repeat(tmp[:, np.newaxis], nCells, 1) + np.cumsum(np.tile(a, self.z.nCells))-2).T
+
+        return VtkData(PolyData(points=nodes, polygons=index))
