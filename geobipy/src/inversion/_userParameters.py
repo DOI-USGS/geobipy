75e9bbcdca519ca302d51f038c9e7278017d1de8
Leon Foks <leonfoks@gmail.com>
2019-02-06 17:13:13 -0700

    updated assertions

--- geobipy/src/inversion/_userParameters.py
+++ geobipy/src/inversion/_userParameters.py
@@ -21,7 +21,7 @@ class _userParameters(myObject):
         """ Check that the specified input parameters match the type of data point"""
 
         if (isinstance(D, FdemDataPoint)):
-            N1 = D.sys.nFreq
+            N1 = D.system[0].nFrequencies
 #            N2 = 2 * N1
             nCalibration = 4
         elif (isinstance(D, TdemDataPoint)):
@@ -33,73 +33,73 @@ class _userParameters(myObject):
 
         # Check the number of Markov chains
         self.nMC = np.int(self.nMC)
-        assert isInt(self.nMC), 'nMC must be a numpy integer'
-        assert self.nMC > 1000, 'Number of Markov Chain iterations nMC must be > 1000'
+        assert isInt(self.nMC), TypeError('nMC must be a numpy integer')
+        assert self.nMC > 1000, ValueError('Number of Markov Chain iterations nMC must be > 1000')
 
         # Check the minumum layer depth
-        assert isinstance(self.minDepth, float), 'minDepth must be a float (preferably np.float64)'
+        assert isinstance(self.minDepth, float), TypeError('minDepth must be a float (preferably np.float64)')
 
         # Check the maximum layer depth
-        assert isinstance(self.maxDepth, float), 'maxDepth must be a float (preferably np.float64)'
+        assert isinstance(self.maxDepth, float), TypeError('maxDepth must be a float (preferably np.float64)')
 
         # Check the minimum layer thickness
         if (not self.minThickness is None):
-            assert isinstance(self.minThickness, float), 'minThickness must be a float (preferably np.float64)'
+            assert isinstance(self.minThickness, float), TypeError('minThickness must be a float (preferably np.float64)')
 
         # Check the maximum number of layers
-        assert isInt(self.maxLayers), 'maxLayers must be an int'
+        assert isInt(self.maxLayers), ValueError('maxLayers must be an int')
 
         # Check the standard deviation of log(rhoa)=log(1+fac)
-        assert isinstance(self.factor, float), 'factor must be a float (preferably np.float64)'
+        assert isinstance(self.factor, float), TypeError('factor must be a float (preferably np.float64)')
 
         # Check the standard deviation
-        assert isinstance(self.GradientStd, float), 'GradientStd must be a float (preferably np.float64)'
+        assert isinstance(self.GradientStd, float), TypeError('GradientStd must be a float (preferably np.float64)')
 
         # Check the reference hitmap if given
         if (not self.referenceHitmap is None):
-            assert isinstance(self.referenceHitmap, Hitmap2D), 'referenceHitmap must be of type Hitmap2D'
+            assert isinstance(self.referenceHitmap, Hitmap2D), TypeError('referenceHitmap must be of type geobipy.Hitmap2D')
 
         # Check the relative Error
-        assert self.relErr.size == D.nSystems, 'Relative Error must have size ' + str(D.nSystems)
+        assert self.relErr.size == D.nSystems, ValueError('Relative Error must have size {}'.format(D.nSystems))
 
         # Check the error floor
-        assert self.addErr.size == D.nSystems, 'Additive Error must have size ' + str(D.nSystems)
+        assert self.addErr.size == D.nSystems, ValueError('Additive Error must have size {}'.format(D.nSystems))
 
         # Check the minimum relative error
-        assert self.rErrMinimum.size == D.nSystems, 'Relative Error minimum must be size ' + str(D.nSystems)
+        assert self.rErrMinimum.size == D.nSystems, ValueError('Relative Error minimum must be size {}'.format(D.nSystems))
 
         # Check the maximum relative error
-        assert self.rErrMaximum.size == D.nSystems, 'Relative Error maximum must be size ' + str(D.nSystems)
+        assert self.rErrMaximum.size == D.nSystems, ValueError('Relative Error maximum must be size {}'.format(D.nSystems))
 
         # Check the range allowed on the data point elevation
-        assert isinstance(self.zRange, float), 'Elevation range must be a float (preferably np.float64)'
+        assert isinstance(self.zRange, float), TypeError('Elevation range must be a float (preferably np.float64)')
 
         # Check the calibration errors if they are used
         if (self.solveCalibration):
-            assert self.calMean.shape == [N1, nCalibration], 'Calibration mean must have shape '+str([N1, nCalibration])
+            assert self.calMean.shape == [N1, nCalibration], ValueError('Calibration mean must have shape {}'.format([N1, nCalibration]))
 
-            assert self.calVar.shape == [N1, nCalibration], 'Calibration variance must have shape '+str([N1, nCalibration])
+            assert self.calVar.shape == [N1, nCalibration], ValueError('Calibration variance must have shape {}'.format([N1, nCalibration]))
 
         # Checking Proposal Variables
         # Check the elevation proposal variance
-        assert isinstance(self.propEl, float), 'Proposal elevation variance must be a float (preferably np.float64)'
+        assert isinstance(self.propEl, float), TypeError('Proposal elevation variance must be a float (preferably np.float64)')
 
         # Check the relative error proposal variance
-        assert self.propRerr.size == D.nSystems, 'Proposal Relative Error variance must be size ' + str(D.nSystems)
+        assert self.propRerr.size == D.nSystems, ValueError('Proposal Relative Error variance must be size {}'.format(D.nSystems))
 
         # Check the calibration proposal variance if they are used
         if (self.solveCalibration):
-            assert self.propCal.shape == [N1, nCalibration], 'Proposal Calibration variance must have shape '+str([N1, nCalibration])
+            assert self.propCal.shape == [N1, nCalibration], ValueError('Proposal Calibration variance must have shape {}'.forma([N1, nCalibration]))
 
         # Check the covariance scaling parameter
-        assert isinstance(self.covScaling, float), 'Covariance scaling must be a float (preferably np.float64)'
+        assert isinstance(self.covScaling, float), TypeError('Covariance scaling must be a float (preferably np.float64)')
 
         # Check the data misfit multiplier factor
-        assert isinstance(self.multiplier, float), 'Data misfit multiplier must be a float (preferably np.float64)'
+        assert isinstance(self.multiplier, float), TypeError('Data misfit multiplier must be a float (preferably np.float64)')
 
         # Checking the Probability Wheel
-        assert isinstance(self.pBirth, float), 'Probability of birth must be a float (preferably np.float64)'
-        assert isinstance(self.pDeath, float), 'Probability of death must be a float (preferably np.float64)'
-        assert isinstance(self.pPerturb, float), 'Probability of perturb must be a float (preferably np.float64)'
-        assert isinstance(self.pNochange, float), 'Probability of no change must be a float (preferably np.float64)'
+        assert isinstance(self.pBirth, float), TypeError('Probability of birth must be a float (preferably np.float64)')
+        assert isinstance(self.pDeath, float), TypeError('Probability of death must be a float (preferably np.float64)')
+        assert isinstance(self.pPerturb, float), TypeError('Probability of perturb must be a float (preferably np.float64)')
+        assert isinstance(self.pNochange, float), TypeError('Probability of no change must be a float (preferably np.float64)')
 
