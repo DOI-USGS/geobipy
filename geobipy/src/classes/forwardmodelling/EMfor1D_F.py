0074966ca84c8f3c7b34de0a1d1d349fc623b662
Foks, Leon <leonfoks@gmail.com>
2019-08-26 11:15:41 -0600

    Added IP frequency domain forward modelling

--- geobipy/src/classes/forwardmodelling/EMfor1D_F.py
+++ geobipy/src/classes/forwardmodelling/EMfor1D_F.py
@@ -5,6 +5,7 @@ June 2015
 """
 import numpy as np
 from .fdemforward1d_fortran import fdemforward1d
+from .ipforward1d_fortran import ipforward1d
 
 def fdem1dfwd(S, mod, z0):
     """ Forward Model a single EM data point from a 1D layered earth conductivity model
@@ -17,7 +18,7 @@ def fdem1dfwd(S, mod, z0):
 
     # Create the indices of the coil orientations for the frequencies.
     tid = S.getTensorID()
-    
+
     tHeight = np.zeros(S.nFrequencies)
     rHeight = np.zeros(S.nFrequencies)
     tMom = np.zeros(S.nFrequencies)
@@ -30,10 +31,42 @@ def fdem1dfwd(S, mod, z0):
         rMom[i] = S.R[i].moment
         rx[i] = S.R[i].x
     scl = tMom * rMom
-       
+
     prd = np.zeros(S.nFrequencies, dtype=np.complex128)
 
     fdemforward1d.forward1d(tid, S.frequencies, tHeight, rHeight, tMom, rx, S.dist, scl, mod.par, mod.thk, prd, S.nFrequencies,  mod.nCells[0])
+
+    return prd
+
+
+def ip1dfwd(S, mod, z0):
+    """ Forward Model a single EM data point from a 1D layered earth conductivity model
+    S:    :EmSystem Class describing the aquisition system
+    mod: : Model1D Class describing the 1D layered earth model
+    z0:  : Altitude of the sensor above the top of the 1D model
+    TODO: SPEED UP THIS CODE
+    """
+    assert z0 <= mod.top, "Sensor altitude must be above the top of the model"
+
+    # Create the indices of the coil orientations for the frequencies.
+    tid = S.getTensorID()
+
+    tHeight = np.zeros(S.nFrequencies)
+    rHeight = np.zeros(S.nFrequencies)
+    tMom = np.zeros(S.nFrequencies)
+    rMom = np.zeros(S.nFrequencies)
+    rx = np.zeros(S.nFrequencies)
+    for i in range(S.nFrequencies):
+        tHeight[i] = -z0 + S.T[i].z
+        rHeight[i] = z0 + S.R[i].z
+        tMom[i] = S.T[i].moment
+        rMom[i] = S.R[i].moment
+        rx[i] = S.R[i].x
+    scl = tMom * rMom
+
+    prd = np.zeros(S.nFrequencies, dtype=np.complex128)
+
+    ipforward1d.forward1d(tid, S.frequencies, tHeight, rHeight, tMom, rx, S.dist, scl, mod.par, mod.thk, prd, S.nFrequencies,  mod.nCells[0])
 
     return prd
 
