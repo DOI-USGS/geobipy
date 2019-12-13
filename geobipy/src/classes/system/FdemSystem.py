9bc08e086ceec0e83988fd899f59d335d52afc75
Leon Foks <nfoks@contractor.usgs.gov>
2019-12-13 12:15:31 -0700

    Added parameters for numba forward modelling

--- geobipy/src/classes/system/FdemSystem.py
+++ geobipy/src/classes/system/FdemSystem.py
@@ -56,11 +56,45 @@ class FdemSystem(myObject):
 
         # StatArray of Loop Separations
         if loopSeparation is None:
-            self.dist = StatArray.StatArray(self._nFrequencies, "Loop Separations", "m")
+            self.loopSeparation = StatArray.StatArray(np.ones(self._nFrequencies), "Loop Separations", "m")
         else:
-            self.dist = StatArray.StatArray(loopSeparation, "Loop Separations", "m")
+            self.loopSeparation = StatArray.StatArray(loopSeparation, "Loop Separations", "m")
 
         self._fileName = systemFilename
+
+        self._w0 = None
+        self._lamda0 = None
+        self._lamda02 = None
+        self._w1 = None
+        self._lamda1 = None
+        self._lamda12 = None
+
+
+    def set_lamdas(self):
+        a0 = np.float64(-8.3885)
+        s0 = np.float64(9.04226468670e-2)
+        a1 = np.float64(-7.91001919)
+        s1 = np.float64(8.7967143957e-2)
+
+        tmp = np.arange(140, dtype = np.float64)
+
+        r = 1.0 / self.loopSeparation
+
+        self._lamda0 = np.empty([self.nFrequencies, 120], dtype=np.float64)
+        self._lamda1 = np.empty([self.nFrequencies, 140], dtype=np.float64)
+
+        l0 = (10.0 ** ((tmp[:120] * s0) + a0))
+        l1 = (10.0 ** ((tmp * s1) + a1))
+
+        for i in range(self.nFrequencies):
+            self._lamda0[i, :] = l0 * r[i]
+            self._lamda1[i, :] = l1 * r[i]
+
+        self._lamda02 = self.lamda0**2.0
+        self._lamda12 = self.lamda1**2.0
+
+        self._w0 = self.set_w0()
+        self._w1 = self.set_w1()
 
 
     @property
@@ -68,7 +102,31 @@ class FdemSystem(myObject):
         return self._frequencies
 
     @property
-    def  nFrequencies(self):
+    def lamda0(self):
+        return self._lamda0
+
+    @property
+    def lamda1(self):
+        return self._lamda1
+
+    @property
+    def lamda02(self):
+        return self._lamda02
+
+    @property
+    def lamda12(self):
+        return self._lamda12
+
+    @property
+    def w0(self):
+        return self._w0
+
+    @property
+    def w1(self):
+        return self._w1
+
+    @property
+    def nFrequencies(self):
         return self._nFrequencies
 
 
@@ -79,7 +137,7 @@ class FdemSystem(myObject):
     def __deepcopy__(self, memo):
         tmp = FdemSystem(nFrequencies = self._nFrequencies)
         tmp._frequencies[:] = self._frequencies
-        tmp.dist[:] = self.dist
+        tmp.dist[:] = self.loopSeparation
         for i in range(self._nFrequencies):
             tmp.T[i] = self.T[i].deepcopy()
             tmp.R[i] = self.R[i].deepcopy()
@@ -123,10 +181,7 @@ class FdemSystem(myObject):
                         line[8]), np.float64(
                         line[9]), np.float64(
                         line[10]))
-                self.dist[j] = np.sqrt(np.power((self.T[j].x - self.R[j].x), 2.0) + np.power((self.T[j].y - self.R[j].y), 2.0))
-                # except Exception:
-                #     raise SystemExit(
-                #         "Could not read from system file:" + fileName + " Line:" + str(j + 2))
+                self.loopSeparation[j] = np.sqrt(np.power((self.T[j].x - self.R[j].x), 2.0) + np.power((self.T[j].y - self.R[j].y), 2.0))
         self.fileName = fileName
 
 
@@ -170,6 +225,7 @@ class FdemSystem(myObject):
                 tid[i] = 9
         return tid
 
+
     def getComponentID(self):
         """ For each coil orientation pair, adds the index of the frequency to the appropriate list
         e.g. two coils at the i$^{th}$ frequency with 'x' as their orientation cause i to be added to the 'xx' list."""
@@ -195,16 +251,19 @@ class FdemSystem(myObject):
                 zz.append(i)
         return np.asarray(xx), np.asarray(xy), np.asarray(xz), np.asarray(yx), np.asarray(yy), np.asarray(yz), np.asarray(zx), np.asarray(zy), np.asarray(zz)
 
+
     def summary(self, out=False):
         """ print a summary of the FdemSystem """
         msg = ("FdemSystem: \n"
                "{} \n"
                "{} \n"
-               "{} \n").format(self._fileName, self._frequencies.summary(True), self.dist.summary(True))
+               "{} \n").format(self._fileName, self._frequencies.summary(True), self.loopSeparation.summary(True))
         return msg if out else print(msg)
+
 
     def hdfName(self):
         return('FdemSystem(0)')
+
 
     def toHdf(self, h5obj, myName):
         """ Write the object to a HDF file """
@@ -213,12 +272,13 @@ class FdemSystem(myObject):
         grp.attrs["repr"] = self.hdfName()
         grp.create_dataset('nFreq', data=self._nFrequencies)
         self._frequencies.toHdf(grp, 'freq')
-        self.dist.toHdf(grp, 'dist')
+        self.loopSeparation.toHdf(grp, 'dist')
         T = grp.create_group('T')
         R = grp.create_group('R')
         for i in range(self._nFrequencies):
             self.T[i].toHdf(T, 'T' + str(i))
             self.R[i].toHdf(R, 'R' + str(i))
+
 
     def fromHdf(self, grp):
         """ Reads the object from a HDF file """
@@ -237,6 +297,7 @@ class FdemSystem(myObject):
             tmp.R[i] = eval(cF.safeEval(item.attrs.get('repr')))
         return tmp
 
+
     def Bcast(self, world, root=0):
         """ Broadcast the FdemSystem using MPI """
         nFreq = myMPI.Bcast(self._nFrequencies, world, root=root)
@@ -244,7 +305,7 @@ class FdemSystem(myObject):
             self = FdemSystem(nFreq)
         this = FdemSystem(nFreq)
         this._frequencies = self._frequencies.Bcast(world, root=root)
-        this.dist = self.dist.Bcast(world, root=root)
+        this.dist = self.loopSeparation.Bcast(world, root=root)
         for i in range(self._nFrequencies):
             this.T[i] = self.T[i].Bcast(world, root=root)
             this.R[i] = self.R[i].Bcast(world, root=root)
@@ -254,7 +315,7 @@ class FdemSystem(myObject):
     def Isend(self, dest, world):
         myMPI.Isend(self._nFrequencies, dest=dest, world=world)
         self._frequencies.Isend(dest=dest, world=world)
-        self.dist.Isend(dest=dest, world=world)
+        self.loopSeparation.Isend(dest=dest, world=world)
         for i in range(self._nFrequencies):
             self.T[i].Isend(dest=dest, world=world)
             self.R[i].Isend(dest=dest, world=world)
@@ -264,13 +325,71 @@ class FdemSystem(myObject):
         nFreq = myMPI.Irecv(source=source, world=world)
         out = FdemSystem(nFreq)
         out._frequencies = out._frequencies.Irecv(source=source, world=world)
-        out.dist = out.dist.Irecv(source=source, world=world)
+        out.loopSeparation = out.dist.Irecv(source=source, world=world)
         for i in range(nFreq):
             out.T[i] = out.T[i].Irecv(source=source, world=world)
             out.R[i] = out.R[i].Irecv(source=source, world=world)
         return out
 
 
+    def set_w0(self):
+        return np.asarray([
+        9.62801364263e-07, -5.02069203805e-06, 1.25268783953e-05, -1.99324417376e-05, 2.29149033546e-05,
+        -2.04737583809e-05, 1.49952002937e-05, -9.37502840980e-06, 5.20156955323e-06, -2.62939890538e-06,
+        1.26550848081e-06, -5.73156151923e-07, 2.76281274155e-07, -1.09963734387e-07, 7.38038330280e-08,
+        -9.31614600001e-09, 3.87247135578e-08, 2.10303178461e-08, 4.10556513877e-08, 4.13077946246e-08,
+        5.68828741789e-08, 6.59543638130e-08, 8.40811858728e-08, 1.01532550003e-07, 1.26437360082e-07,
+        1.54733678097e-07, 1.91218582499e-07, 2.35008851918e-07, 2.89750329490e-07, 3.56550504341e-07,
+        4.39299297826e-07, 5.40794544880e-07, 6.66136379541e-07, 8.20175040653e-07, 1.01015545059e-06,
+        1.24384500153e-06, 1.53187399787e-06, 1.88633707689e-06, 2.32307100992e-06, 2.86067883258e-06,
+        3.52293208580e-06, 4.33827546442e-06, 5.34253613351e-06, 6.57906223200e-06, 8.10198829111e-06,
+        9.97723263578e-06, 1.22867312381e-05, 1.51305855976e-05, 1.86329431672e-05, 2.29456891669e-05,
+        2.82570465155e-05, 3.47973610445e-05, 4.28521099371e-05, 5.27705217882e-05, 6.49856943660e-05,
+        8.00269662180e-05, 9.85515408752e-05, 1.21361571831e-04, 1.49454562334e-04, 1.84045784500e-04,
+        2.26649641428e-04, 2.79106748890e-04, 3.43716968725e-04, 4.23267056591e-04, 5.21251001943e-04,
+        6.41886194381e-04, 7.90483105615e-04, 9.73420647376e-04, 1.19877439042e-03, 1.47618560844e-03,
+        1.81794224454e-03, 2.23860214971e-03, 2.75687537633e-03, 3.39471308297e-03, 4.18062141752e-03,
+        5.14762977308e-03, 6.33918155348e-03, 7.80480111772e-03, 9.61064602702e-03, 1.18304971234e-02,
+        1.45647517743e-02, 1.79219149417e-02, 2.20527911163e-02, 2.71124775541e-02, 3.33214363101e-02,
+        4.08864842127e-02, 5.01074356716e-02, 6.12084049407e-02, 7.45146949048e-02, 9.00780900611e-02,
+        1.07940155413e-01, 1.27267746478e-01, 1.46676027814e-01, 1.62254276550e-01, 1.68045766353e-01,
+        1.52383204788e-01, 1.01214136498e-01, -2.44389126667e-03, -1.54078468398e-01, -3.03214415655e-01,
+        -2.97674373379e-01, 7.93541259524e-03, 4.26273267393e-01, 1.00032384844e-01, -4.94117404043e-01,
+        3.92604878741e-01, -1.90111691178e-01, 7.43654896362e-02, -2.78508428343e-02, 1.09992061155e-02,
+        -4.69798719697e-03, 2.12587632706e-03, -9.81986734159e-04, 4.44992546836e-04, -1.89983519162e-04,
+        7.31024164292e-05, -2.40057837293e-05, 6.23096824846e-06, -1.12363896552e-06, 1.04470606055e-07], dtype=np.float64)
+
+
+    def set_w1(self):
+        return np.asarray([
+        -6.76671159511e-14, 3.39808396836e-13, -7.43411889153e-13, 8.93613024469e-13, -5.47341591896e-13,
+        -5.84920181906e-14, 5.20780672883e-13, -6.92656254606e-13, 6.88908045074e-13, -6.39910528298e-13,
+        5.82098912530e-13, -4.84912700478e-13, 3.54684337858e-13, -2.10855291368e-13, 1.00452749275e-13,
+        5.58449957721e-15, -5.67206735175e-14, 1.09107856853e-13, -6.04067500756e-14, 8.84512134731e-14,
+        2.22321981827e-14, 8.38072239207e-14, 1.23647835900e-13, 1.44351787234e-13, 2.94276480713e-13,
+        3.39965995918e-13, 6.17024672340e-13, 8.25310217692e-13, 1.32560792613e-12, 1.90949961267e-12,
+        2.93458179767e-12, 4.33454210095e-12, 6.55863288798e-12, 9.78324910827e-12, 1.47126365223e-11,
+        2.20240108708e-11, 3.30577485691e-11, 4.95377381480e-11, 7.43047574433e-11, 1.11400535181e-10,
+        1.67052734516e-10, 2.50470107577e-10, 3.75597211630e-10, 5.63165204681e-10, 8.44458166896e-10,
+        1.26621795331e-09, 1.89866561359e-09, 2.84693620927e-09, 4.26886170263e-09, 6.40104325574e-09,
+        9.59798498616e-09, 1.43918931885e-08, 2.15798696769e-08, 3.23584600810e-08, 4.85195105813e-08,
+        7.27538583183e-08, 1.09090191748e-07, 1.63577866557e-07, 2.45275193920e-07, 3.67784458730e-07,
+        5.51470341585e-07, 8.26916206192e-07, 1.23991037294e-06, 1.85921554669e-06, 2.78777669034e-06,
+        4.18019870272e-06, 6.26794044911e-06, 9.39858833064e-06, 1.40925408889e-05, 2.11312291505e-05,
+        3.16846342900e-05, 4.75093313246e-05, 7.12354794719e-05, 1.06810848460e-04, 1.60146590551e-04,
+        2.40110903628e-04, 3.59981158972e-04, 5.39658308918e-04, 8.08925141201e-04, 1.21234066243e-03,
+        1.81650387595e-03, 2.72068483151e-03, 4.07274689463e-03, 6.09135552241e-03, 9.09940027636e-03,
+        1.35660714813e-02, 2.01692550906e-02, 2.98534800308e-02, 4.39060697220e-02, 6.39211368217e-02,
+        9.16763946228e-02, 1.28368795114e-01, 1.73241920046e-01, 2.19830379079e-01, 2.51193131178e-01,
+        2.32380049895e-01, 1.17121080205e-01, -1.17252913088e-01, -3.52148528535e-01, -2.71162871370e-01,
+        2.91134747110e-01, 3.17192840623e-01, -4.93075681595e-01, 3.11223091821e-01, -1.36044122543e-01,
+        5.12141261934e-02, -1.90806300761e-02, 7.57044398633e-03, -3.25432753751e-03, 1.49774676371e-03,
+        -7.24569558272e-04, 3.62792644965e-04, -1.85907973641e-04, 9.67201396593e-05, -5.07744171678e-05,
+        2.67510121456e-05, -1.40667136728e-05, 7.33363699547e-06, -3.75638767050e-06, 1.86344211280e-06,
+        -8.71623576811e-07, 3.61028200288e-07, -1.05847108097e-07, -1.51569361490e-08, 6.67633241420e-08,
+        -8.33741579804e-08, 8.31065906136e-08, -7.53457009758e-08, 6.48057680299e-08, -5.37558016587e-08,
+        4.32436265303e-08, -3.37262648712e-08, 2.53558687098e-08, -1.81287021528e-08, 1.20228328586e-08,
+        -7.10898040664e-09, 3.53667004588e-09, -1.36030600198e-09, 3.52544249042e-10, -4.53719284366e-11], dtype=np.float64)
 
 
 
