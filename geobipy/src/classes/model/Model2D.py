af2a2622d04633ad83f7042be15b49b8be9d2d08
Leon Foks <nfoks@contractor.usgs.gov>
2020-06-02 11:08:02 -0600

    minor

--- geobipy/src/classes/model/Model2D.py
+++ geobipy/src/classes/model/Model2D.py
@@ -23,7 +23,7 @@ import matplotlib.gridspec as gridspec
 
 class Model2D(Model):
     """ 2D Histogram class that can update and plot efficiently.
-    
+
     Class extension to the RectilinearMesh2D.  The mesh defines the x and y axes, while the Histogram2D manages the counts.
 
     Histogram2D(x, y, name, units)
@@ -43,7 +43,7 @@ class Model2D(Model):
     -------
     out : Histogram2D
         2D histogram
-    
+
     """
 
     def __init__(self, mesh, values=None, name=None, units=None):
@@ -56,19 +56,17 @@ class Model2D(Model):
         if values is None:
             self._values = StatArray.StatArray([self.y.nCells, self.x.nCells], name=name, units=units)
         else:
-            self._values = StatArray.StatArray(values)
-            self._values.name = name if not name is None
-            self._values.units = units if not units is None
+            self._values = StatArray.StatArray(values, name, units)
 
 
     @property
     def mesh(self):
         return self._mesh
 
-    
+
     def axisMean(self, log=None, axis=0):
         """Gets the mean along the given axis.
-        
+
         This is not the true mean of the original samples. It is the best estimated mean using the binned counts multiplied by the axis bin centres.
 
         Parameters
@@ -80,9 +78,9 @@ class Model2D(Model):
         -------
         out : geobipy.StatArray
             The means along the axis.
-        
+
         """
-        
+
         if axis == 0:
             t = np.sum(np.repeat(self.x.cellCentres[np.newaxis, :], self.y.nCells, 0) * self._values, 1)
         else:
@@ -92,7 +90,7 @@ class Model2D(Model):
         i = np.where(s > 0.0)[0]
         tmp = np.zeros(t.size)
         tmp[i] = t[i] / s[i]
-        
+
         if log:
             tmp, dum = _logSomething(tmp, log=log)
 
@@ -101,7 +99,7 @@ class Model2D(Model):
 
     def axisMedian(self, log=None, axis=0):
         """Gets the median for the specified axis.
-        
+
         Parameters
         ----------
         log : 'e' or float, optional
@@ -122,7 +120,7 @@ class Model2D(Model):
         """Return an opacity between 0 and 1 based on the difference between confidence invervals of the hitmap.
 
         Higher ranges in confidence map to less opaqueness.
-        
+
         Parameters
         ----------
         percent : float
@@ -142,7 +140,7 @@ class Model2D(Model):
 
     def axisPercentage(self, percent, log=None, axis=0):
         """Gets the percentage of the CDF for the specified axis.
-        
+
         Parameters
         ----------
         percent : float
@@ -185,7 +183,7 @@ class Model2D(Model):
         """Return a transparency value between 0 and 1 based on the difference between confidence invervals of the hitmap.
 
         Higher ranges in confidence are mapped to more transparency.
-        
+
         Parameters
         ----------
         percent : float
@@ -214,16 +212,16 @@ class Model2D(Model):
 
         self.gs = gridspec.GridSpec(5, 5)
         self.gs.update(wspace=0.3, hspace=0.3)
-        plt.subplot(self.gs[1:, :4]) 
+        plt.subplot(self.gs[1:, :4])
         self.pcolor(noColorbar = True, **kwargs)
 
-        ax = plt.subplot(self.gs[:1, :4]) 
+        ax = plt.subplot(self.gs[:1, :4])
         h = self.axisHistogram(0).plot(**kwargs)
         plt.xlabel(''); plt.ylabel('')
         plt.xticks([]); plt.yticks([])
         ax.spines["left"].set_visible(False)
 
-        ax = plt.subplot(self.gs[1:, 4:]) 
+        ax = plt.subplot(self.gs[1:, 4:])
         h = self.axisHistogram(0).plot(rotate=True, **kwargs)
         plt.ylabel(''); plt.xlabel('')
         plt.yticks([]); plt.xticks([])
@@ -238,7 +236,7 @@ class Model2D(Model):
         -------
         out : geobipy.Histogram2D
             2D histogram with the new intervals.
-        
+
         See Also
         --------
         geobipy.RectilinearMesh2D.intervalMean : for parameter information
@@ -257,8 +255,8 @@ class Model2D(Model):
 
 
     def pcolor(self, **kwargs):
-        """Plot the Histogram2D as an image 
-        
+        """Plot the Histogram2D as an image
+
         Other Parameters
         ----------------
         alpha : scalar or array_like, optional
@@ -282,10 +280,10 @@ class Model2D(Model):
         grid : bool, optional
             Plot the grid
         noColorbar : bool, optional
-            Turn off the colour bar, useful if multiple customPlots plotting routines are used on the same figure.   
+            Turn off the colour bar, useful if multiple customPlots plotting routines are used on the same figure.
         trim : bool, optional
             Set the x and y limits to the first and last non zero values along each axis.
-        
+
         """
         self._values.pcolor(x=self.x.cellEdges, y=self.y.cellEdges, **kwargs)
 
