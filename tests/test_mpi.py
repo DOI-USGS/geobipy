e4dde27e9e40dd9d40381899f4965a178a6e528c
Leon Foks <nfoks@usgs.gov>
2021-10-22 20:21:00 -0600

    MPI tests working

--- tests/test_mpi.py
+++ tests/test_mpi.py
@@ -7,19 +7,24 @@ from geobipy import Data
 from geobipy import DataPoint
 from geobipy import FdemData
 from geobipy import TdemData
+from geobipy import TempestData
 from geobipy import FdemSystem
 from geobipy import TdemSystem
 from geobipy import FdemDataPoint
 from geobipy import TdemDataPoint
+from geobipy import Tempest_datapoint
 from geobipy.src.base import MPI as myMPI
 
+##############################################################################
+# Init
+##############################################################################
 world = MPI.COMM_WORLD
 
 rank = world.rank
 master = rank == 0
 size = world.size
 
-#assert size == 4, Exception("Please use 4 cores to test")
+assert size == 4, Exception("Please use 4 cores to test")
 
 dataPath = "..//documentation_source//source//examples//supplementary//Data//"
 
@@ -31,7 +36,9 @@ starts, chunks = myMPI.loadBalance1D_shrinkingArrays(N, size)
 i0 = starts[rank]
 i1 = i0 + chunks[rank]
 
-### Test base geobipy.MPI routines
+##############################################################################
+# Test base geobipy.MPI routines
+##############################################################################
 
 # data type
 dt = None
@@ -107,7 +114,9 @@ z = myMPI.IrecvFromLeft(world)
 tst = [[3, 3, 3, 3], 0, [1, 1], [2, 2, 2]]
 assert np.all(z == tst[rank]), Exception("e) Could not Isend/Irecv Left to Right. Rank {}".format(rank))
 
-## Test the StatArray routines
+##############################################################################
+# Test the StatArray routines
+##############################################################################
 x = StatArray((rank+1) * np.arange(N, dtype=np.float64), "name", "units")
 
 # Bcast
@@ -160,7 +169,13 @@ xSave = np.arange(N)
 ySave = np.arange(N) + 10.0
 zSave = np.arange(N) + 100.0
 
-### Test the PointCloud3D
+dSave = np.repeat(np.arange(N)[:, np.newaxis], 4, 1) + np.asarray([1000,2000,3000,4000])
+sSave = np.full([N, 4], 5.0)
+pSave = np.ones([N, 4])
+
+##############################################################################
+# Test the Pointcloud3D
+##############################################################################
 if master:
     pc = PointCloud3D(xSave, ySave, zSave)
 else:
@@ -175,21 +190,21 @@ pc1 = pc.Scatterv(starts, chunks, world)
 
 assert np.all(pc1.x == xSave[i0:i1]) and np.all(pc1.y == ySave[i0:i1]) and np.all(pc1.z == zSave[i0:i1]), Exception("Could not use PointCloud3D.Scatterv. Rank {}".format(rank))
 
-dSave = np.repeat(np.arange(N)[:, np.newaxis], 4, 1) + np.asarray([1000,2000,3000,4000])
-sSave = np.full([N, 4], 5.0)
-pSave = np.ones([N, 4])
-## Test the Data class
+##############################################################################
+# Test the Data class
+##############################################################################
 if master:
     ncps = np.asarray([2,2])
     data = Data(channels_per_system=ncps, x=xSave, y=ySave, z=zSave, data=dSave, std=sSave, predictedData=pSave, channelNames=['t1', 't2', 't3', 't4'])
 else:
-    data = Data(0)
+    data = Data()
 
 # Bcast
 data1 = data.Bcast(world)
 
 assert np.all(data1.x == xSave) and np.all(data1.y == ySave) and np.all(data1.z == zSave), Exception("Could not use Data.Bcast. Rank {}".format(rank))
 assert np.all(data1.data == dSave) and np.all(data1.std == sSave) and np.all(data1.predictedData == pSave), Exception("Could not use Data.Bcast. Rank {}".format(rank))
+
 
 # Scatterv
 data1 = data.Scatterv(starts, chunks, world)
@@ -202,14 +217,18 @@ y = None
 if master:
     y = data.datapoint(0)
     for i in range(1, size):
-        data.datapoint(i).Isend(dest=i, world=world)
+        dp = data.datapoint(i)
+        dp.Isend(dest=i, world=world)
 else:
     y = DataPoint.Irecv(0, world)
 
-assert np.all(y.data == dSave[rank, :]) and np.all(y.std == sSave[rank, :]) and np.all(y.predictedData == pSave[rank, :]), Exception("Could not use Data.Isend/Irecv. Rank {}".format(rank))
 
-### Test Frequency Domain Data
-fdSave = FdemData.read_csv(dataPath+"Resolve2.txt", dataPath+"FdemSystem2.stm")
+assert np.all(y.data == dSave[rank, :]) and np.all(y.predictedData == pSave[rank, :]), Exception("Could not use Data.Isend/Irecv. Rank {}".format(rank))
+
+##############################################################################
+# Test Frequency Domain Data
+##############################################################################
+fdSave = FdemData.read_csv(dataPath+"Resolve_small.txt", dataPath+"FdemSystem2.stm")
 if master:
     fd = fdSave
 else:
@@ -231,7 +250,7 @@ assert np.allclose(fd1.data, fdSave.data[i0:i1, :], equal_nan=True), Exception("
 
 # Point by Point read in and send
 if master:
-    fd = FdemData._initialize_sequential_reading(dataPath+"Resolve2.txt", dataPath+"FdemSystem2.stm")
+    fd = FdemData._initialize_sequential_reading(dataPath+"Resolve_small.txt", dataPath+"FdemSystem2.stm")
 
 # Send and recieve a single datapoint from the file.
 if master:
@@ -244,12 +263,12 @@ else:
 
 assert np.allclose(fdp.data, fdSave.data[rank, :], equal_nan=True), Exception("Could not use FdemData.Isend/Irecv. Rank {}".format(rank))
 
-#Testing pre-read in system classes
-systems = FdemSystem.read(dataPath+"FdemSystem2.stm")
+
+systems = FdemSystem.read(dataPath + "FdemSystem2.stm")
 
 # Send and recieve a single datapoint from the file.
 if master:
-    fd = FdemData._initialize_sequential_reading(dataPath+"Resolve2.txt", dataPath+"FdemSystem2.stm")
+    fd = FdemData._initialize_sequential_reading(dataPath+"Resolve_small.txt", dataPath+"FdemSystem2.stm")
 
     fdp = fd._read_record()
     for i in range(1, size):
@@ -260,9 +279,12 @@ else:
 
 assert np.allclose(fdp.data, fdSave.data[rank, :], equal_nan=True), Exception("Could not use FdemData.Isend/Irecv, with pre-existing system class. Rank {}".format(rank))
 
+##############################################################################
+# Test TdemData
+##############################################################################
 
-tdSave = TdemData.read_csv([dataPath+"Skytem_High.txt", dataPath+"Skytem_Low.txt"], [dataPath+"SkytemHM-SLV.stm", dataPath+"SkytemLM-SLV.stm"])
-### Test Time Domain Data
+tdSave = TdemData.read_csv([dataPath+"Skytem_HM_small.txt", dataPath+"Skytem_LM_small.txt"], [dataPath+"SkytemHM-SLV.stm", dataPath+"SkytemLM-SLV.stm"])
+# # ### Test Time Domain Data
 if master:
     td = tdSave
 else:
@@ -284,7 +306,7 @@ assert np.allclose(td1.data, tdSave.data[i0:i1, :], equal_nan=True), Exception("
 
 # Send and recieve a single datapoint from the file.
 if master:
-    td = TdemData._initialize_sequential_reading([dataPath+"Skytem_High.txt", dataPath+"Skytem_Low.txt"], [dataPath+"SkytemHM-SLV.stm", dataPath+"SkytemLM-SLV.stm"])
+    td = TdemData._initialize_sequential_reading([dataPath+"Skytem_HM_small.txt", dataPath+"Skytem_LM_small.txt"], [dataPath+"SkytemHM-SLV.stm", dataPath+"SkytemLM-SLV.stm"])
 
     tdp = td._read_record()
     for i in range(1, size):
@@ -303,16 +325,72 @@ for s in sysPath:
 
 # Send and recieve a single datapoint from the file.
 if master:
-    td = TdemData._initialize_sequential_reading([dataPath+"Skytem_High.txt", dataPath+"Skytem_Low.txt"], [dataPath+"SkytemHM-SLV.stm", dataPath+"SkytemLM-SLV.stm"])
+    td = TdemData._initialize_sequential_reading([dataPath+"Skytem_HM_small.txt", dataPath+"Skytem_LM_small.txt"], [dataPath+"SkytemHM-SLV.stm", dataPath+"SkytemLM-SLV.stm"])
     tdp = td._read_record()
     for i in range(1, size):
         tdp1 = td._read_record()
-        tdp1.Isend(dest=i, world=world, systems=systems)
+        tdp1.Isend(dest=i, world=world, system=systems)
 else:
-    tdp = TdemDataPoint.Irecv(source=0, world=world, systems=systems)
+    tdp = TdemDataPoint.Irecv(source=0, world=world, system=systems)
 
 assert np.allclose(tdp.data, tdSave.data[rank, :], equal_nan=True), Exception("Could not use TdemData.Isend/Irecv. Rank {}".format(rank))
 
+##############################################################################
+# Test Tempest Data
+##############################################################################
+tdSave = TempestData.read_netcdf(dataPath+"Tempest.nc", dataPath+"Tempest.stm", indices=np.s_[:10])
+# ### Test Time Domain Data
+if master:
+    td = tdSave
+else:
+    td = TempestData()
+
+# Bcast
+td1 = td.Bcast(world)
+
+assert np.allclose(td1.data, tdSave.data, equal_nan=True), Exception("Could not use TempestData.Bcast. Rank {}".format(rank))
+
+# # Scatterv
+starts, chunks = myMPI.loadBalance1D_shrinkingArrays(tdSave.nPoints, size)
+i0 = starts[rank]
+i1 = i0 + chunks[rank]
+
+td1 = td.Scatterv(starts, chunks, world)
+
+assert np.allclose(td1.data, tdSave.data[i0:i1, :], equal_nan=True), Exception("Could not use TempestData.Scatterv. Rank {}".format(rank))
+assert np.allclose(td1.primary_field, tdSave.primary_field[i0:i1, :], equal_nan=True), Exception("Could not use TempestData.Scatterv. Rank {}".format(rank))
+
+# Send and recieve a single datapoint from the file.
+if master:
+    td = TempestData._initialize_sequential_reading(dataPath+"Tempest.nc", dataPath+"Tempest.stm")
+
+    tdp = td._read_record(0)
+    for i in range(1, size):
+        tdp1 = td._read_record(i)
+        tdp1.Isend(dest=i, world=world)
+else:
+    tdp = Tempest_datapoint.Irecv(source=0, world=world)
+
+assert np.allclose(tdp.primary_field, tdSave.primary_field[rank, :], equal_nan=True), Exception("Could not use TdemData.Isend/Irecv. Rank {}".format(rank))
+assert np.allclose(tdp.secondary_field, tdSave.secondary_field[rank, :], equal_nan=True), Exception("Could not use TdemData.Isend/Irecv. Rank {}".format(rank))
+assert np.allclose(tdp.data, tdSave.data[rank, :], equal_nan=True), Exception("Could not use TdemData.Isend/Irecv. Rank {}".format(rank))
+
+# Testing pre-read in system classes
+system = TdemSystem.read(dataPath+"Tempest.stm")
+
+# Send and recieve a single datapoint from the file.
+if master:
+    td = TempestData._initialize_sequential_reading(dataPath+"Tempest.nc", dataPath+"Tempest.stm")
+    tdp = td._read_record(0)
+    for i in range(1, size):
+        tdp1 = td._read_record(i)
+        tdp1.Isend(dest=i, world=world, system=system)
+else:
+    tdp = Tempest_datapoint.Irecv(source=0, world=world, system=system)
+
+assert np.allclose(tdp.primary_field, tdSave.primary_field[rank, :], equal_nan=True), Exception("Could not use TdemData.Isend/Irecv. Rank {}".format(rank))
+assert np.allclose(tdp.secondary_field, tdSave.secondary_field[rank, :], equal_nan=True), Exception("Could not use TdemData.Isend/Irecv. Rank {}".format(rank))
+assert np.allclose(tdp.data, tdSave.data[rank, :], equal_nan=True), Exception("Could not use TdemData.Isend/Irecv. Rank {}".format(rank))
 
 print("All tests passed. Rank {}".format(rank), flush=True)
 
