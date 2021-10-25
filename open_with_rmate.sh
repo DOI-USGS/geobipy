59bd4cbbceef61f1a4ad4bccddb0d9d41ce02382
Leon Foks <leonfoks@gmail.com>
2021-10-25 12:16:08 -0500

    minor

--- open_with_rmate.sh
+++ open_with_rmate.sh
@@ -1,18 +1,18 @@
 
-rmate /lustre/projects/css/csas/nfoks/geobipy/geobipy/src/classes/pointcloud/Point.py
-rmate /lustre/projects/css/csas/nfoks/geobipy/geobipy/src/classes/pointcloud/PointCloud3D.py
+rmate ./geobipy/src/classes/pointcloud/Point.py
+rmate ./geobipy/src/classes/pointcloud/PointCloud3D.py
 
-rmate /lustre/projects/css/csas/nfoks/geobipy/geobipy/src/classes/data/datapoint/DataPoint.py
-rmate /lustre/projects/css/csas/nfoks/geobipy/geobipy/src/classes/data/dataset/Data.py
+rmate ./geobipy/src/classes/data/datapoint/DataPoint.py
+rmate ./geobipy/src/classes/data/dataset/Data.py
 
-rmate /lustre/projects/css/csas/nfoks/geobipy/geobipy/src/classes/data/datapoint/EmDataPoint.py
+rmate ./geobipy/src/classes/data/datapoint/EmDataPoint.py
 
-rmate /lustre/projects/css/csas/nfoks/geobipy/geobipy/src/classes/data/datapoint/FdemDataPoint.py
-rmate /lustre/projects/css/csas/nfoks/geobipy/geobipy/src/classes/data/dataset/FdemData.py
+rmate ./geobipy/src/classes/data/datapoint/FdemDataPoint.py
+rmate ./geobipy/src/classes/data/dataset/FdemData.py
 
-rmate /lustre/projects/css/csas/nfoks/geobipy/geobipy/src/classes/data/datapoint/TdemDataPoint.py
-rmate /lustre/projects/css/csas/nfoks/geobipy/geobipy/src/classes/data/dataset/TdemData.py
+rmate ./geobipy/src/classes/data/datapoint/TdemDataPoint.py
+rmate ./geobipy/src/classes/data/dataset/TdemData.py
 
-rmate /lustre/projects/css/csas/nfoks/geobipy/geobipy/src/classes/data/datapoint/Tempest_datapoint.py
-rmate /lustre/projects/css/csas/nfoks/geobipy/geobipy/src/classes/data/dataset/TempestData.py
+rmate ./geobipy/src/classes/data/datapoint/Tempest_datapoint.py
+rmate ./geobipy/src/classes/data/dataset/TempestData.py
 # Data
