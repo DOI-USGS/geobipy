6b0fa2833e799b3af215cf719eb91b60f8166ae4
Leon Foks <leonfoks@gmail.com>
2022-05-06 06:50:35 -0700

    minor

--- geobipy/src/base/utilities.py
+++ geobipy/src/base/utilities.py
@@ -7,10 +7,16 @@ import h5py
 from sklearn.mixture import GaussianMixture
 from smm import SMM
 
-from numba import (jit, float64)
-_numba_settings = {'nopython': True, 'nogil': False, 'fastmath': True, 'cache': True}
+from numba import (njit, jit, float64)
+_jit_settings = {'nopython': True, 'nogil': False, 'fastmath': True, 'cache': True}
+_njit_settings = {'nogil': False, 'fastmath': True, 'cache': True}
 
-@jit(**_numba_settings)
+from numba.pycc import CC
+
+cc = CC('test')
+cc.verbose = True
+@njit(**_njit_settings)
+@cc.export('bresenham', 'f8[:, :](f8[:], f8[:])')
 def bresenham(x, y):
     n_segments = np.int32(len(x) - 1)
     nTmp = np.int32(0)
@@ -382,7 +388,7 @@ def cosSin1(x, y, a, p):
     """Simple function for creating tests. """
     return x * (1.0 - x) * np.cos(a * np.pi * x) * np.sin(a * np.pi * y**p)**p
 
-@jit(**_numba_settings)
+@njit(**_njit_settings)
 def reorder_3d_for_pyvista(values):
     shp = np.shape(values)
     arr = np.zeros(shp[0] * shp[1] * shp[2], dtype=float64)
@@ -543,14 +549,39 @@ def LogDet(A, N=1.0):
         d = np.linalg.slogdet(A)
         return d[0] * d[1]
 
+@njit(**_njit_settings)
+def set_rows_at(values, indices, out):
+    for i in range(len(indices)):
+        out[indices[i], :] = values[i, :]
+    return out
+
+@njit(**_njit_settings)
+def set_columns_at(values, indices, out):
+    for i in range(len(indices)):
+        out[:, indices[i]] = values[:, i]
+    return out
 
 def rolling_window(a, window):
     shape = a.shape[:-1] + (a.shape[-1] - window + 1, window)
     strides = a.strides + (a.strides[-1],)
     return np.lib.stride_tricks.as_strided(a, shape=shape, strides=strides)
 
-@jit(**_numba_settings)
+@njit(**_njit_settings)
 def smooth(x, a):
+    """Smooth x by an LTI gaussian filter, forwards and backwards pass.
+
+    Parameters
+    ----------
+    x : array_like
+        signal to process
+    a : scalar between 0.0 and 1.0
+        Weight
+
+    Returns
+    -------
+    out : array_like
+        Smoothed signal
+    """
     n = len(x)
     b = 1.0 - a
     sx = 1.0
@@ -937,7 +968,6 @@ def _power(values, exponent=None):
         The values to power exponent
 
     """
-
     if exponent is None:
         return values
     if exponent == 'e':
@@ -959,10 +989,10 @@ def safeEval(string):
         string = string.replace('EmLoop', 'CircularLoop')
         return string
 
-    allowed = ('Histogram',
+    allowed = (
+    'Histogram',
     'Model',
     'Model1D',
-    'Hitmap',
     'TempestData',
     'TdemData',
     'FdemData',
@@ -972,6 +1002,7 @@ def safeEval(string):
     'TdemSystem',
     'FdemSystem',
     'CircularLoop',
+    'CircularLoops',
     'RectilinearMesh')
 
     if (any(x in string for x in allowed)):
