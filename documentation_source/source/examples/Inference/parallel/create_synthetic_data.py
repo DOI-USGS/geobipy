f55fb0f685f18dc9266640bfe9adc9fbdd11f591
Leon Foks <leonfoks@gmail.com>
2023-03-08 11:38:03 -0800

    change to tempest error levels

--- documentation_source/source/examples/Inference/parallel/create_synthetic_data.py
+++ documentation_source/source/examples/Inference/parallel/create_synthetic_data.py
@@ -53,7 +53,7 @@ def make_figure(ds, model, title):
     d = ds.datapoint(0); plt.subplot(splt[0, 0]); d.plot();
 
     ax = plt.subplot(splt[0, 1]);
-    ds.plot_data(xAxis='x');
+    ds.plot_data(x='x');
     ax.get_legend().remove();
     ax1 = plt.subplot(splt[1, 1], sharex=ax);
     model.pcolor(flipY=True, log=10);
@@ -213,7 +213,7 @@ def create_tempest(model, output_suffix):
                     yaw = np.random.uniform(low=-5.0, high=5.0, size=model.x.nCells),
                     radius=np.full(model.x.nCells, fill_value=ds.system[0].loopRadius()))
 
-    ds.relative_error = np.repeat(np.r_[0.02, 0.02][None, :], model.x.nCells, 0)
+    ds.relative_error = np.repeat(np.r_[0.005, 0.005][None, :], model.x.nCells, 0)
     add_error = np.r_[0.011474, 0.012810, 0.008507, 0.005154, 0.004742, 0.004477, 0.004168, 0.003539, 0.003352, 0.003213, 0.003161, 0.003122, 0.002587, 0.002038, 0.002201,
                     0.007383, 0.005693, 0.005178, 0.003659, 0.003426, 0.003046, 0.003095, 0.003247, 0.002775, 0.002627, 0.002460, 0.002178, 0.001754, 0.001405, 0.001283]
     ds.additive_error = np.repeat(add_error[None, :], model.x.nCells, 0)
@@ -260,7 +260,7 @@ if __name__ == '__main__':
     for k in keys:
         wedge_model = create_model(k)
 
-        create_resolve(wedge_model, k)
-        create_skytem(wedge_model, k)
+        # create_resolve(wedge_model, k)
+        # create_skytem(wedge_model, k)
         # create_aerotem(wedge_model, k)
         create_tempest(wedge_model, k)
