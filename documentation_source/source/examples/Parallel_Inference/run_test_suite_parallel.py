f54a9798f0a22bf1ef80f29cc3a25f9e982699bb
Leon Foks <leonfoks@gmail.com>
2023-10-02 08:46:53 -0700

    minor

--- documentation_source/source/examples/Parallel_Inference/run_test_suite_parallel.py
+++ documentation_source/source/examples/Parallel_Inference/run_test_suite_parallel.py
@@ -59,7 +59,7 @@ def parallel_mpi(data_type, model_type, output_directory):
     # Start keeping track of time.
     t0 = MPI.Wtime()
 
-    prng = myMPI.get_prng(MPI.Wtime, world=world)
+    prng = myMPI.get_prng(world=world)
 
     inference3d = Inference3D(data, prng=prng, world=world)
     inference3d.create_hdf5(directory=output_directory, **kwargs)
