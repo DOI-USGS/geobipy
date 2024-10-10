ae62542ada7bfb340b7f7172d8915e5df243a856
Leon Foks <nfoks@contractor.usgs.gov>
2024-10-09 18:44:30 -0700

    minor

--- update_version.py
+++ update_version.py
@@ -4,12 +4,39 @@ Parser = argparse.ArgumentParser(description="update_version", formatter_class=a
 Parser.add_argument('version', help='version')
 args = Parser.parse_args()
 
-with open("setup.py", "r") as f:
-    lines = f.readlines()
+with open("documentation_source/source/conf.py", "r") as f:
+        lines = f.readlines()
+with open("documentation_source/source/conf.py", "w") as f:
+        for line in lines:
+            if "version =" in line:
+                f.write('version = "{}"\n'.format(args.version))
+            elif "release =" in line:
+                f.write('release = "{}"\n'.format(args.version))
+            else:
+                f.write(line)
 
-with open("setup.py", "w") as f:
-    for line in lines:
-        if "__version__ =" in line:
-            f.write("__version__ = '{}'\n".format(args.version))
-        else:
-            f.write(line)
+try:
+    with open("setup.py", "r") as f:
+        lines = f.readlines()
+
+    with open("setup.py", "w") as f:
+        for line in lines:
+            if "__version__ =" in line:
+                f.write('__version__ = "{}"\n'.format(args.version))
+            else:
+                f.write(line)
+except:
+    pass
+
+try:
+    with open("pyproject.toml", "r") as f:
+        lines = f.readlines()
+
+    with open("pyproject.toml", "w") as f:
+        for line in lines:
+            if "version =" in line:
+                f.write('version = "{}"\n'.format(args.version))
+            else:
+                f.write(line)
+except:
+    pass
