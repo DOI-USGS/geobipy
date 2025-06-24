55e355be7659aa82122514aacf6a9b5af58561bb
Leon Foks <nfoks@contractor.usgs.gov>
2025-06-24 08:52:01 -0700

    big simplification on dumping arrays to HDF5

--- geobipy/src/base/HDF/hdfWrite.py
+++ geobipy/src/base/HDF/hdfWrite.py
@@ -1,4 +1,4 @@
-from numpy import ndim, size, integer
+from numpy import atleast_1d, ndim, s_, size, integer
 def write_nd(arr, h5obj, myName, index=None):
     """Writes a numpy array to a preallocated dataset in a h5py group object
 
@@ -23,7 +23,7 @@ def write_nd(arr, h5obj, myName, index=None):
 def write_nd_nonindexed(arr, h5obj, myName):
 
     nd = ndim(arr)
-    assert nd <= 6, ValueError('The number of dimensions to write must be <= 6')
+    # assert nd <= 6, ValueError('The number of dimensions to write must be <= 6')
 
     # Pull the group
     ds = h5obj[myName]
@@ -37,25 +37,9 @@ def write_nd_nonindexed(arr, h5obj, myName):
 
     # Write the entire array to appropriate locations in memory. No way to combine this into a function for N dimensions
     # Automatically fills memory from the beginning index in each dimension.
-    if (nd == 1):
-        ds[:arr.size] = arr
 
-    s = arr.shape
-    if(nd == 2):
-        ds[:s[0], :s[1]] = arr
-
-    elif(nd == 3):
-        ds[:s[0], :s[1], :s[2]] = arr
-
-    elif(nd == 4):
-        ds[:s[0], :s[1], :s[2], :s[3]] = arr
-
-    elif(nd == 5):
-        ds[:s[0], :s[1], :s[2], :s[3], :s[4]] = arr
-
-    elif(nd == 6):
-        ds[:s[0], :s[1], :s[2], :s[3], :s[4], :s[5]] = arr
-
+    slic = tuple([s_[:size(arr, axis=i)] for i in range(nd)])
+    ds[slic] = arr
 
 def write_nd_indexed(arr, h5obj, myName, index):
 
@@ -79,88 +63,9 @@ def write_nd_indexed(arr, h5obj, myName, index):
     assert ndi + nda == ds.ndim or (ndi == 1 and nda == 1), ValueError("index must have {} dimensions".format(ds.ndim - nda))
 
     i = index
-    if ndi == 0:
-        if (nda == 0):
-            ds[i, 0] = arr
-
-        elif (nda == 1):
-            ds[i, :s] = arr
-
-        elif(nda == 2):
-            ds[i, :s[0], :s[1]] = arr
-
-        elif(nda == 3):
-            ds[i, :s[0], :s[1], :s[2]] = arr
-
-        elif(nda == 4):
-            ds[i, :s[0], :s[1], :s[2], :s[3]] = arr
-
-        elif(nda == 5):
-            ds[i, :s[0], :s[1], :s[2], :s[3], :s[4]] = arr
-
-        elif(nda == 6):
-            ds[i, :s[0], :s[1], :s[2], :s[3], :s[4], :s[5]] = arr
-
-    elif ndi == 1:
-        if not isinstance(i, (int, integer)):
-            i = i[0]
-        if (nda == 1):
-            if ndim(ds) == 1:
-                ds[i] = arr
-            else:
-                ds[i, :s] = arr
-
-        elif(nda == 2):
-            ds[i, :s[0], :s[1]] = arr
-
-        elif(nda == 3):
-            ds[i, :s[0], :s[1], :s[2]] = arr
-
-        elif(nda == 4):
-            ds[i, :s[0], :s[1], :s[2], :s[3]] = arr
-
-        elif(nda == 5):
-            ds[i, :s[0], :s[1], :s[2], :s[3], :s[4]] = arr
-
-        elif(nda == 6):
-            ds[i, :s[0], :s[1], :s[2], :s[3], :s[4], :s[5]] = arr
-
-
-    elif ndi == 2:
-        if (nda == 1):
-            ds[i[0], i[1], :s] = arr
-
-        elif(nda == 2):
-            ds[i[0], i[1], :s[0], :s[1]] = arr
-
-        elif(nda == 3):
-            ds[i[0], i[1], :s[0], :s[1], :s[2]] = arr
-
-        elif(nda == 4):
-            ds[i[0], i[1], :s[0], :s[1], :s[2], :s[3]] = arr
-
-        elif(nda == 5):
-            ds[i[0], i[1], :s[0], :s[1], :s[2], :s[3], :s[4]] = arr
-
-        elif(nda == 6):
-            ds[i[0], i[1], :s[0], :s[1], :s[2], :s[3], :s[4], :s[5]] = arr
-
-
-    elif ndi == 3:
-        if (nda == 1):
-            ds[i[0], i[1], i[2], :s] = arr
-
-        elif(nda == 2):
-            ds[i[0], i[1], i[2], :s[0], :s[1]] = arr
-
-        elif(nda == 3):
-            ds[i[0], i[1], i[2], :s[0], :s[1], :s[2]] = arr
-
-        elif(nda == 4):
-            ds[i[0], i[1], i[2], :s[0], :s[1], :s[2], :s[3]] = arr
-
-        elif(nda == 5):
-            ds[i[0], i[1], i[2], :s[0], :s[1], :s[2], :s[3], :s[4]] = arr
-
-        elif(nda == 6):
-            ds[i[0], i[1], i[2], :s[0], :s[1], :s[2], :s[3], :s[4], :s[5]] = arr
+    if (nda == 0):
+        ds[i, 0] = arr
+    else:
+        i = i.item() if i.size == 1 else i
+        slic = tuple([i] + [s_[:size(arr, axis=j)] for j in range(nda)])
+        ds[slic] = arr
